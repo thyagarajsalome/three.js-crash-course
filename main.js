fecf3a51cb616b91887ed0cc57365e8a3d6a5795
thyagaraja <thyagarajsalome@gmailcom>
2024-10-03 14:52:54 +0530

    fonts added

--- main.js
+++ main.js
@@ -85,5 +85,3 @@ const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.from(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 tl.fromTo(",title", { opacity: 0 }, { opacity: 1 });
-
-//mouse animation color
