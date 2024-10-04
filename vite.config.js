63bc95b53cea1f82b0a7311e6afda7d8e183a000
thyagaraja <thyagarajsalome@gmailcom>
2024-10-04 18:16:26 +0530

    missing files added

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // Your Vite configuration options
+  build: {
+    outDir: "dist",
+  },
+  // Add any other necessary configurations
+});
